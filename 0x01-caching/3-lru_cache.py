f27d87c30a38298707085f5522555448c8567095
salmaelfadil <salmaelfadil@aucegypt.edu>
2024-03-16 12:40:43 +0000

    .

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python3
-"""LIFO caching module."""
+"""LRU caching module."""
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
-class LIFOCache(BaseCaching):
-    """LIFO caching"""
+class LRUCache(BaseCaching):
+    """LRU caching"""
     def __init__(self):
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """"assigns the value of the key"""
         if key is None or item is None:
             return
         if key not in self.cache_data.keys():
+            #self.cache_datai[key] = item
+            #self.cache_data.move_to_end(key)
             if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem()
-                print("DISCARD:", last_key)
+                d_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", d_key)
         self.cache_data[key] = item
+        self.cache_data.move_to_end(key)
 
-    def get(self, key):
+    def get(self, ikey):
         """getter method"""
         return self.cache_data.get(key, None)
